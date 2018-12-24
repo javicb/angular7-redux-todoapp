46d0bb346daf332d60b21a45c487f64023cbea13
Javier Cruz <javi.cb@gmail.com>
2018-12-24 13:29:08 +0100

    first commit

---
+++ src/app/todo/todo-footer/todo-footer.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { filtrosValidos, SetFiltroAction } from '../../filter/filter.actions';
+import { BorrarAllTodoAction } from '../todo.actions';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducers';
+import { Todo } from '../model/todo.model';
+
+@Component({
+  selector: 'app-todo-footer',
+  templateUrl: './todo-footer.component.html',
+  styles: []
+})
+export class TodoFooterComponent implements OnInit {
+
+  pendientes: number;
+
+  filtrosValidos: filtrosValidos [] = ['todos', 'completados', 'pendientes'] ;
+  filtroActual: filtrosValidos;
+
+  constructor( private store: Store<AppState> ) { }
+
+  ngOnInit() {
+
+    this.store.subscribe( state => {
+
+      this.contarPendientes( state.todos );
+      this.filtroActual = state.filtro;
+
+    });
+
+  }
+
+  cambiarFiltro( nuevoFiltro: filtrosValidos ) {
+
+    const accion = new SetFiltroAction(nuevoFiltro);
+    this.store.dispatch( accion );
+
+  }
+
+  contarPendientes( todos: Todo[] ) {
+
+    this.pendientes = todos.filter( todo => !todo.completado ).length;
+
+  }
+
+  borrarTodo() {
+
+    const accion = new BorrarAllTodoAction();
+    this.store.dispatch( accion );
+
+  }
+
+}
